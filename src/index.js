630818bb3ad11724226740c7c8df845536540ab4
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2022-11-18 17:27:07 +0700

    Function and class component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,61 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+function Form (props) {
+    function handleClickButton (e) {
+        alert(e.target.value);
+    }
+    return (
+        <button value="1" onClick={handleClickButton}>
+            {props.nameButton}
+        </button>
+    );
+}
+const form = <Form nameButton="Button nè"/>
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class Button extends React.Component {
+    state = {
+        name : "Button con nè"
+    }
+    handleClickButton() {
+        this.setState({name : "Không phải button đâu"});
+    }
+
+    styleButton = {
+        display : "flex",
+    }
+
+    render() {
+        return (
+            <div style={this.styleButton}>
+                <button value="12345" onClick={this.handleClickButton.bind(this)}>
+                    {this.props.nameButton}
+                </button>
+                {/* <ButtonChild name={this.state.name}/> */}
+            </div>
+        );
+    };
+}
+
+class ButtonChild extends Button {
+    handleClickButtonChild(e) {
+        alert(1234556);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClickButtonChild}>
+                {this.props.name}
+            </button>
+        );
+    }
+}
+
+const button = <Button nameButton="Button đấy" />
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  button
 );
 
 // If you want to start measuring performance in your app, pass a function
