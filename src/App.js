0ab1be71110ffd888879c50455513e56c82ce325
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2022-11-24 13:48:34 +0700

    handle get data form checked radio input with state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, {useState} from "react";
 
+const datas = ["Nguyễn Văn A", "Nguyễn Văn B", "Nguyễn Thị C"];
+
 function App() {
+
+  const [checked, setChecked] = useState(2);
+
+  const handleSubmit = () => {
+    console.log(datas[checked]);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div style={{padding : 32}}>
+      {datas.map((data, index) => (
+          <div key={index}>
+            <input id={"data" + index} type='radio' checked={index === checked ? true : false} onChange={() => setChecked(index)}/>
+            <label htmlFor={"data" + index}>{data}</label>
+          </div>
+      ))
+      }
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
   );
 }
 
