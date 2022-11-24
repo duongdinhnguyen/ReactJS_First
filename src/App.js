3f69decded20efddb2dc301485fa6f13202f833c
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2022-11-24 15:29:24 +0700

    TodoList with two way binding and useState

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import React, {useState} from "react";
 
-const datas = ["Nguyễn Văn A", "Nguyễn Văn B", "Nguyễn Thị C"];
-
 function App() {
 
-  const [checked, setChecked] = useState([]);
+  const [todoList, setTodoList] = useState([]);
+  const [todo, setTodo] = useState('');
 
-  const handleSubmit = () => {
-    console.log(checked);
+  const handleAddTodo = () => {
+    setTodoList((preTodo) => [...preTodo, todo]);
+    setTodo('');
   }
 
-  const handleChecked = (index) => {
-    setChecked((preChecked) => {
-      if (preChecked.includes(index)) {
-        return preChecked.filter(item => item !== index);
-      }
-      else {
-        return [...preChecked, index];
-      }
-    });
+  const changeInput = (e) => {
+    setTodo(e.target.value);
   }
-
-  const isChecked = (index) => checked.includes(index);
   return (
     <div style={{padding : 32}}>
-      {datas.map((data, index) => (
-          <div key={index}>
-            <input id={"data" + index} type='checkbox' checked={ isChecked(index) } onChange={() => handleChecked(index)}/>
-            <label htmlFor={"data" + index}>{data}</label>
-          </div>
-      ))
-      }
-      <button onClick={handleSubmit}>Submit</button>
+      <input onChange={changeInput} value={todo}/>
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {
+          todoList.map((item, index) => (
+            <li key={index}>
+                {item}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
