d3a9f71683c0dd7494ec73b99b2e4a3a10448137
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2022-11-24 14:31:10 +0700

    handle get data call api with two way binding on form input multiple checkbox

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ const datas = ["Nguyễn Văn A", "Nguyễn Văn B", "Nguyễn Thị C"];
 
 function App() {
 
-  const [checked, setChecked] = useState(2);
+  const [checked, setChecked] = useState([]);
 
   const handleSubmit = () => {
-    console.log(datas[checked]);
+    console.log(checked);
   }
 
+  const handleChecked = (index) => {
+    setChecked((preChecked) => {
+      if (preChecked.includes(index)) {
+        return preChecked.filter(item => item !== index);
+      }
+      else {
+        return [...preChecked, index];
+      }
+    });
+  }
+
+  const isChecked = (index) => checked.includes(index);
   return (
     <div style={{padding : 32}}>
       {datas.map((data, index) => (
           <div key={index}>
-            <input id={"data" + index} type='radio' checked={index === checked ? true : false} onChange={() => setChecked(index)}/>
+            <input id={"data" + index} type='checkbox' checked={ isChecked(index) } onChange={() => handleChecked(index)}/>
             <label htmlFor={"data" + index}>{data}</label>
           </div>
       ))
