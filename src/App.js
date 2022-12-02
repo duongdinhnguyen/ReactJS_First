4bc43de7a9dd9554b63308815b33ae6ea1eeccec
duonqdinhnquyen <nguyen.dinhduong@jvb-corp.com>
2022-12-02 10:38:55 +0700

    hook useReducer basic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
+
+// Initial Value
+const initialValue = 0
+
+// defined const
+const UP = 'up'
+const DOWN = 'down'
+
+// custom function action reducer
+const reducer = (state, action) => {
+  switch (action) {
+    case UP:
+      return state + 1
+    case DOWN:
+      return state - 1
+  
+    default:
+      break;
+  }
+
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialValue) // Khởi tạo reducer
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>{count}</h1>
+      <br></br>
+      <button onClick={() => dispatch(UP)}>Up</button> {/* nhận vào action UP, tương ứng sẽ là tăng count lên và update state*/}
+      <button onClick={() => dispatch(DOWN)}>Down</button>
+    </>
   );
 }
 
